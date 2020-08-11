84add27dd2f9b05e553b5362d9694ad4f4ed7f79
hui-shao <31568606+hui-shao@users.noreply.github.com>
2020-08-11 14:30:08 +0800

    [Init.] 添加源码文件
    主要实现了登录和删除枯树功能

---
+++ avalon.py
@@ -0,0 +1,343 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Name: Avalon Standard Command Line I/O Framework
+Author: K4YT3X
+Date Created: March 20, 2017
+Last Modified: October 12, 2019
+
+Licensed under the GNU Lesser General Public License Version 3 (GNU LGPL v3),
+    available at: https://www.gnu.org/licenses/lgpl-3.0.txt
+
+(C) 2017-2019 K4YT3X
+
+Description: this framework tries to make printing messages and
+getting user input easier it includes most UNIX terminal background
+and foreground colors.
+"""
+
+# built-in imports
+import sys
+import threading
+
+PLATFORM = sys.platform
+
+# thirt-party imports
+# if system is windows, initialize colorama
+# which translates UNIX console color sequences
+# into windows color sequences
+if PLATFORM == 'win32':
+    from colorama import init
+
+    init()
+
+# if system is UNIX
+# import syslog for writting logs into syslog
+else:
+    import syslog
+
+VERSION = '1.8.2'
+
+
+class Avalon:
+    """ Avalon Standard Input/Output Framework
+
+    The avalon framework provides convenient and beautiful methods
+    to handle command line user input or program output. It's based on
+    Unix console colors, and is made compatible with the Windows platform
+    with the colorama library.
+    """
+
+    # optional thread lock
+    thread_lock = None
+
+    class FG:
+        """ Foreground Colors
+
+        This class contains all foreground colors.
+        Access colors via Avalon.FG.Color.
+        """
+
+        # Standard colors
+        BL = '\033[30m'  # Black
+        R = '\033[31m'  # Red
+        G = '\033[32m'  # Green
+        Y = '\033[33m'  # Yellow
+        B = '\033[34m'  # Blue
+        M = '\033[35m'  # Magenta
+        C = '\033[36m'  # Cyan
+
+        # Light colors
+        LGR = '\033[37m'  # Light Grey
+        DGR = '\033[90m'  # Dark Grey
+        LR = '\033[91m'  # Light Red
+        LG = '\033[92m'  # Light Green
+        LY = '\033[93m'  # Light Yellow
+        LB = '\033[94m'  # Light Blue
+        LM = '\033[95m'  # Light Magenta
+        LC = '\033[96m'  # Light Cyan
+        W = '\033[97m'  # White
+
+    class BG:
+        """ Foreground Colors
+
+        This class contains all background colors.
+        Access colors via Avalon.BG.Colors.
+        """
+
+        BL = '\033[40m'  # Black
+        R = '\033[41m'  # Red
+        G = '\033[42m'  # Green
+        Y = '\033[43m'  # Yellow
+        B = '\033[44m'  # Blue
+        M = '\033[45m'  # Magenta
+        C = '\033[46m'  # Cyan
+        LGR = '\033[47m'  # Light Grey
+        DGR = '\033[100m'  # Dark Grey
+        LR = '\033[101m'  # Light Red
+        LG = '\033[102m'  # Light Green
+        LY = '\033[103m'  # Light Yellow
+        LB = '\033[104m'  # Light Blue
+        LM = '\033[105m'  # Light Magenta
+        LC = '\033[106m'  # Light Cyan
+        WT = '\033[107m'  # White
+
+    class FM:
+        """ Formatting Sequences
+
+        This class contains all formatting-related
+        sequences, such as bold or italic.
+        Access formats via Avalon.FM.Color.
+        """
+
+        # SET
+        BD = '\033[1m'  # Bold
+        DM = '\033[2m'  # Dim
+        UN = '\033[4m'  # Underlined
+        BL = '\033[5m'  # Blink
+        RV = '\033[7m'  # Reverse
+        HD = '\033[8m'  # Hidden
+
+        # RESET
+        RST = '\033[0m'  # Reset ALL
+        RBD = '\033[21m'  # Bold
+        RDM = '\033[22m'  # Dim
+        RUN = '\033[24m'  # Underlined
+        RBL = '\033[25m'  # Blink
+        RRV = '\033[27m'  # Reverse
+        RHD = '\033[28m'  # Hidden
+
+    @staticmethod
+    def get_version():
+        return VERSION
+
+    @staticmethod
+    def _print(msg, file, end):
+        """ thread-safe print method
+
+        This is a simple thread-safe print method. If a thread lock
+        is provided, then the lock will be acquired before printing the
+        message, and will be released after printing the message.
+
+        Arguments:
+            msg {string} -- message to print
+
+        Keyword Arguments:
+            thread_lock {threading.Lock()} -- thread lock to use (default: {None})
+        """
+        if isinstance(threading.Lock(), type(Avalon.thread_lock)):
+            Avalon.thread_lock.acquire()
+            print(msg, file=file, end=end)
+            Avalon.thread_lock.release()
+        else:
+            print(msg, file=file, end=end)
+
+    @staticmethod
+    def info(msg, log=False, front="", end="\n", file=sys.stdout):
+        """ print regular information
+
+        Arguments:
+            msg {str} -- message to print
+
+        Keyword Arguments:
+            log {bool} -- Ture logs message to syslog on Linux (default: {False})
+            file {_io.TextIOWrapper} -- pipe to write output to (default: {sys.stdout})
+        """
+        Avalon._print(f'{front}{Avalon.FG.G}[+] INFO: {str(msg)}{Avalon.FM.RST}', file=file, end=end)
+        if log and PLATFORM != 'win32':
+            syslog.syslog(syslog.LOG_INFO, msg)
+
+    @staticmethod
+    def time_info(msg, log=False, front="", end="\n", file=sys.stdout):
+        """ print regular information with time stamp
+
+        Arguments:
+            msg {str} -- message to print
+
+        Keyword Arguments:
+            log {bool} -- Ture logs message to syslog on Linux (default: {False})
+            file {_io.TextIOWrapper} -- pipe to write output to (default: {sys.stdout})
+        """
+        import datetime
+        Avalon._print(
+            f'{front}{Avalon.FM.RST}{str(datetime.datetime.now())}{Avalon.FG.G} [+] INFO: {str(msg)}{Avalon.FM.RST}',
+            file=file, end=end)
+        if log and PLATFORM != 'win32':
+            syslog.syslog(syslog.LOG_INFO, msg)
+
+    @staticmethod
+    def debug_info(msg, log=True, front="", end="\n", file=sys.stderr):
+        """ print information for debugging
+
+        Arguments:
+            msg {str} -- message to print
+
+        Keyword Arguments:
+            log {bool} -- Ture logs message to syslog on Linux (default: {True})
+            file {_io.TextIOWrapper} -- pipe to write output to (default: {sys.stderr})
+        """
+        import datetime
+        Avalon._print(f'{front}{Avalon.FG.DGR}{str(datetime.datetime.now())} [+] INFO: {str(msg)}{Avalon.FM.RST}',
+                      file=file, end=end)
+        if log and PLATFORM != 'win32':
+            syslog.syslog(syslog.LOG_DEBUG, msg)
+
+    @staticmethod
+    def warning(msg, log=False, front="", end="\n", file=sys.stderr):
+        """ print a warning message
+
+        Arguments:
+            msg {str} -- message to print
+
+        Keyword Arguments:
+            log {bool} -- Ture logs message to syslog on Linux (default: {False})
+            file {_io.TextIOWrapper} -- pipe to write output to (default: {sys.stderr})
+        """
+        Avalon._print(f'{front}{Avalon.FG.Y}{Avalon.FM.BD}[^] WARNING: {str(msg)}{Avalon.FM.RST}', file=file, end=end)
+        if log and PLATFORM != 'win32':
+            syslog.syslog(syslog.LOG_WARNING, msg)
+
+    @staticmethod
+    def error(msg, log=True, front="", end="\n", file=sys.stderr):
+        """ print an error messages
+
+        Arguments:
+            msg {str} -- message to print
+
+        Keyword Arguments:
+            log {bool} -- Ture logs message to syslog on Linux (default: {True})
+            file {_io.TextIOWrapper} -- pipe to write output to (default: {sys.stderr})
+        """
+        Avalon._print(f'{front}{Avalon.FG.R}{Avalon.FM.BD}[!] ERROR: {str(msg)}{Avalon.FM.RST}', file=file, end=end)
+        if log and PLATFORM != 'win32':
+            syslog.syslog(syslog.LOG_WARNING, msg)
+
+    @staticmethod
+    def debug(msg, log=True, front="", end="\n", file=sys.stderr):
+        """ print a debug message
+
+        Arguments:
+            msg {str} -- message to print
+
+        Keyword Arguments:
+            log {bool} -- Ture logs message to syslog on Linux (default: {True})
+            file {_io.TextIOWrapper} -- pipe to write output to (default: {sys.stderr})
+        """
+        Avalon._print(f'{front}{Avalon.FG.R}{Avalon.FM.RDM}[*] DEBUG: {str(msg)}{Avalon.FM.RST}', file=file, end=end)
+        if log and PLATFORM != 'win32':
+            syslog.syslog(syslog.LOG_DEBUG, msg)
+
+    @staticmethod
+    def gets(msg, default=None, batch=False, front="", end="", file=sys.stdout):
+        """ gets user input
+
+        Arguments:
+            msg {str} -- message to print
+
+        Keyword Arguments:
+            default {str} -- default value to return (default: {None})
+            batch {bool} -- if True, return default value (default: {False})
+            file {_io.TextIOWrapper} -- pipe to write output to (default: {sys.stderr})
+
+        Returns:
+            str -- user input
+        """
+
+        # if batch is set, return the default value
+        if batch:
+            return default
+
+        print(f'{front}{Avalon.FG.C}{Avalon.FM.BD}[?] USER: {str(msg)}{Avalon.FM.RST}', end=end, file=file)
+
+        # get user input
+        user_input = input()
+
+        # if user input is empty and default value is set
+        # return default value
+        # otherwise return empty string
+        if user_input == '' and default is not None:
+            print()
+            return default
+        else:
+            return user_input
+
+    @staticmethod
+    def ask(msg, default=False, batch=False, front="", end="", ):
+        """ gets a True / False answer from user
+
+        This method will ask user a question that will
+        require a true / false answer. Pressing enter without
+        entering anything will return the default value.
+
+        Arguments:
+            msg {str} -- message to print
+
+        Keyword Arguments:
+            default {bool} -- default value to return when input is blank (default: {False})
+            batch {bool} -- if True, return default value (default: {False})
+
+        Raises:
+            TypeError: raised when default is not bool type
+
+        Returns:
+            bool -- user's answer
+        """
+
+        # if batch is set, return the default value
+        if batch:
+            return default
+
+        # if default value is False
+        # when user gives no input, return False
+        elif default is False:
+            while True:
+                answer = Avalon.gets(f'{str(msg)} [y/N]: ', front=front, end=end)
+                if answer == '':
+                    print()
+                    return False
+                elif answer[0].upper() == 'N':
+                    return False
+                elif answer[0].upper() == 'Y':
+                    return True
+                else:
+                    Avalon.error('Invalid Input!')
+
+        # if default value is True
+        # when user gives no input, return True
+        elif default is True:
+            while True:
+                answer = Avalon.gets(f'{str(msg)} [Y/n]: ', front=front, end=end)
+                if answer == '':
+                    print()
+                    return True
+                elif answer[0].upper() == 'Y':
+                    return True
+                elif answer[0].upper() == 'N':
+                    return False
+                else:
+                    Avalon.error('Invalid Input!')
+
+        # if default is neither True or False
+        # in another word, not isinstance(default, bool)
+        else:
+            raise TypeError('invalid type for positional argument: \' default\'')
---
+++ main.py
@@ -0,0 +1,335 @@
+# -*- coding: UTF-8 -*-
+# @Time    : 2020/8/3 23:58
+# @Author  : Hui-Shao
+# %% imports
+import json
+import os
+import shutil
+import sys
+import time
+from urllib import parse
+
+import requests
+import toml
+from requests.cookies import RequestsCookieJar
+
+from avalon import Avalon
+
+
+# %% 建立用于保存用户文件的目录
+def makedir():
+    if not os.path.exists("UserFiles"):
+        os.mkdir("UserFiles")
+    else:
+        pass
+
+
+# %% 读取配置文件
+def read_config():
+    Avalon.info("读取配置文件中……")
+    global config, username, passwd, uid, remember_token
+    config_file = sys.argv[1] if len(sys.argv) > 1 else "config.toml"
+    try:
+        with open(config_file, "r", encoding="utf-8") as f:
+            config = toml.load(f)
+    except UnicodeEncodeError:
+        with open(config_file, "r", encoding="gbk") as f:
+            config = toml.load(f)
+    except IOError:
+        Avalon.error(f"无法加载{config_file}, 请检查文件是否存在, 文件名是否正确")
+        return False
+    except Exception:
+        Avalon.error(f"无法加载{config_file}, 其他错误")
+        return False
+    else:
+        username = config["user"]["username"]
+        passwd = config["user"]["password"]
+        uid = config["user"]["uid"]
+        remember_token = config["user"]["remember_token"]
+        return True
+
+
+# %% 写配置文件
+def write_config():
+    config_file = sys.argv[1] if len(sys.argv) > 1 else "config.toml"
+    config_2 = config.copy()
+    config_2["user"].update({"uid": uid, "remember_token": remember_token})
+    try:
+        f = open(config_file, "w+", encoding="utf-8")
+        toml.dump(config_2, f)
+        f.close()
+    except IOError:
+        Avalon.error("IOError")
+        return False
+    else:
+        return True
+
+
+# %% 自定义的HttpRequest模块
+def _requests(_method, _url, _data=None, _ex_hea=None):
+    url_parse = parse.urlparse(_url)
+    cookie_jar = RequestsCookieJar()
+    str_json = json.dumps(_data)  # 注意, 不可使用str()方法转换
+    hea = {
+        "Accept": "*/*",
+        "Accept-Encoding": "br, gzip, deflate",
+        "Accept-Language": "zh-Hans-CN;q=1",
+        "Connection": "keep-alive",
+        "Content-Length": str(len(json.dumps(_data))),
+        "Content-Type":
+            "application/json; charset=utf-8",
+        "Host": url_parse.netloc,
+        "User-Agent": "Forest/342 (iPhone; iOS 12.1.2; Scale/3.00)"
+    }
+    if _ex_hea:
+        hea.update(_ex_hea)
+    if len(remember_token):
+        cookie_jar.set("remember_token", remember_token, domain=url_parse.netloc)
+    if _method.upper() == 'GET':
+        res = requests.get(_url, data=str_json, headers=hea, cookies=cookie_jar)
+    elif _method.upper() == 'POST':
+        res = requests.post(_url, data=str_json, headers=hea, cookies=cookie_jar)
+    elif _method.upper() == 'PUT':
+        res = requests.put(_url, data=str_json, headers=hea, cookies=cookie_jar)
+    elif _method.upper() == 'DELETE':
+        res = requests.delete(_url, data=str_json, headers=hea, cookies=cookie_jar)
+    else:
+        print('TypeError')
+        return None
+    return res
+
+
+# %% 登录 获取uid, remember_token 等信息
+def login():
+    Avalon.info("正在登录...", front="\n")
+    global remember_token, uid
+    data = {
+        "session": {
+            "email": username,
+            "password": passwd,
+        },
+        "seekruid": ""
+    }
+    r = _requests("post", "https://c88fef96.forestapp.cc/api/v1/sessions", data, {})
+    if r.status_code != 200:
+        Avalon.error("登录可能失败")
+        return False
+    else:
+        id_info = json.loads(r.text)
+        remember_token = id_info["remember_token"]
+        uid = id_info["user_id"]
+        Avalon.info("登录成功, 欢迎你~: %s" % id_info["user_name"])
+        return True
+
+
+# %% 登出
+def logout():
+    Avalon.info("正在登出...", front="\n")
+    global uid, remember_token
+    url = f"https://c88fef96.forestapp.cc/api/v1/sessions/signout?seekrua=android_cn-4.20.1&seekruid={uid}"
+    r = _requests("delete", url, {}, {})
+    if r.status_code != 200:
+        Avalon.error(f"登出可能失败")
+        return False
+    else:
+        Avalon.info(f"登出成功")
+        uid = 0
+        remember_token = ""
+        write_config()
+        try:
+            shutil.rmtree("UserFiles")
+        except IOError:
+            Avalon.warning("删除UserFiles目录失败, 在切换用户前务必手动删除！")
+            return False
+        return True
+
+
+# %% 获取树种列表  树木的gid, 名称, 哪些已解释
+def get_plants():
+    file_name = "plants.json"
+
+    def run():
+        Avalon.info(f"正在获取已种植列表 ({file_name})", front="\n")
+        if os.path.exists(f"UserFiles/{file_name}"):
+            Avalon.info(f"发现本地已存在 {file_name} , 进行读取...")
+            get_from_local()
+        else:
+            Avalon.warning(f"在本地未发现 {file_name} , 尝试从服务器端获取...")
+            get_from_server()
+
+    def get_from_local():
+        global plants
+        with open(f"UserFiles/{file_name}", "r", encoding="utf-8") as f:
+            plants = json.loads(f.read())
+        Avalon.info(f"从本地获取 已种植列表 ({file_name}) 成功")
+
+    def get_from_server():
+        global plants
+        url = f"https://c88fef96.forestapp.cc/api/v1/plants?seekrua=android_cn-4.20.1&seekruid={uid}"
+        r = _requests("get", url, {}, {})
+        if r.status_code == 200:
+            plants = json.loads(r.text)
+            Avalon.info(f"从服务器端获取 已种植列表 ({file_name}) 成功")
+            with open(f"UserFiles/{file_name}", "w", encoding="utf-8") as f:
+                f.write(r.text)
+            return True
+        else:
+            Avalon.error(f"从服务端获取 {file_name} 可能失败")
+            return False
+
+    run()
+
+
+# %% 获取树木种类列表
+def get_coin_tree_types():
+    file_name = "coin_tree_types.json"
+
+    def run():
+        Avalon.info(f"正在获取树木种类列表 ({file_name})", front="\n")
+        if os.path.exists(f"UserFiles/{file_name}"):
+            Avalon.info(f"发现本地已存在 {file_name} , 进行读取...")
+            get_from_local()
+        else:
+            Avalon.warning(f"在本地未发现 {file_name} , 尝试从服务器端获取...")
+            get_from_server()
+
+    def get_from_local():
+        global coin_tree_types
+        with open(f"UserFiles/{file_name}", "r", encoding="utf-8") as f:
+            coin_tree_types = json.loads(f.read())
+        Avalon.info(f"从本地获取 已种植列表 ({file_name}) 成功")
+
+    def get_from_server():
+        global coin_tree_types
+        url = f"https://c88fef96.forestapp.cc/api/v1/coin_tree_types?seekrua=android_cn-4.20.1&seekruid={uid}"
+        r = _requests("get", url)
+        if r.status_code == 200:
+            coin_tree_types = json.loads(r.text)
+            Avalon.info(f"从服务器端获取 树木种类列表 ({file_name}) 成功")
+            with open(f"UserFiles/{file_name}", "w", encoding="utf-8") as f:
+                f.write(r.text)
+            return True
+        else:
+            Avalon.error(f"从服务端获取 {file_name} 可能失败")
+            return False
+
+    run()
+
+
+# %% 模拟观看广告删除枯树
+def remove_plants_by_rewarded_ad():
+    def run():
+        dead_plants = find_dead_plant_id()
+        if not dead_plants:
+            Avalon.warning("当前plants.json中未发现枯树")
+            return True
+        for tree in dead_plants:
+            Avalon.info(f"正在删除枯树...编号:%d  种类代码:%d  数量:%d棵" % (tree["id"], tree["tree_type_gid"], tree["tree_count"]),
+                        front="\n")
+            ad_session_token = get_ad_session_token()
+            ad_token = get_ad_token(ad_session_token)
+            if ad_token is False:
+                return False
+            else:
+                time.sleep(1)
+                if not simulate_watch_ad(ad_token, ad_session_token):
+                    return False
+                else:
+                    time.sleep(0.5)
+                    return delete_plants(tree["id"])
+
+    # 从plants.json中查找枯树的id
+    def find_dead_plant_id():
+        Avalon.info("正在查找枯树的id...用时可能较长..请稍候...", front="\n")
+        dead_trees = []
+        if len(plants) <= 0:
+            get_plants()
+        for a in plants:
+            if a["is_success"]:
+                continue
+            else:
+                dead_trees.append(a)
+                continue
+        return dead_trees
+
+    def get_ad_session_token():
+        r = _requests("post", "https://receipt-system.seekrtech.com/projects/1/ad_sessions", {}, {})
+        if (r.status_code == 201) or (r.status_code == 200):
+            Avalon.info("获取 ad_session_token 成功")
+            return json.loads(r.text)["token"]
+        else:
+            Avalon.error("获取 ad_session_token 失败")
+            return False
+
+    def get_ad_token(_ad_session_token):
+        data = {"ad_session_token": f"{_ad_session_token}"}
+        r = _requests("post", "https://receipt-system.seekrtech.com/sv_rewarded_ad_views", data, {})
+        if (r.status_code == 201) or (r.status_code == 200):
+            Avalon.info("获取 ad_token 成功")
+            return json.loads(r.text)["token"]
+        else:
+            Avalon.error("获取 ad_token 失败")
+            return False
+
+    def simulate_watch_ad(_ad_token, _ad_session_token):
+        url_1 = f"https://receipt-system.seekrtech.com/sv_rewarded_ad_views/{_ad_token}/watched?seekrua=android_cn-4.20.1&seekruid={uid}"
+        data_1 = {"ad_session_token": f"{_ad_session_token}"}
+        r_1 = _requests("put", url_1, data_1)
+        if r_1.status_code != 200:
+            Avalon.error("模拟观看广告失败!  位置: 1")
+            return False
+        time.sleep(0.3)
+        url_2 = f"https://receipt-system.seekrtech.com/sv_rewarded_ad_views/{_ad_token}/claim?seekrua=android_cn-4.20.1&seekruid={uid}"
+        data_2 = {"ad_session_token": f"{_ad_session_token}", "user_id": uid}
+        r_2 = _requests("put", url_2, data_2)
+        if r_2.status_code != 200:
+            Avalon.error("模拟观看广告失败!  位置: 2")
+            return False
+        else:
+            Avalon.info("模拟观看广告成功")
+            return True
+
+    def delete_plants(_id):
+        if uid <= 0:
+            Avalon.error("uid错误")
+            return False
+        url = f"https://c88fef96.forestapp.cc/api/v1/plants/{_id}/remove_plant_by_rewarded_ad?seekrua=android_cn-4.20.1&seekruid={uid}"
+        r = _requests("delete", url)
+        if r.status_code != 200:
+            Avalon.error("删除种植记录失败!")
+            if r.status_code == 422:
+                Avalon.error("原因: Cannot find the product with given id")
+            elif r.status_code == 403:
+                Avalon.error("原因: 403 Forbidden. 请检查cookie")
+            else:
+                pass
+            return False
+        else:
+            Avalon.info("删除种植记录成功")
+            return True
+
+    run()
+
+
+def main():
+    makedir()
+    read_config()
+    if uid * len(remember_token) == 0:
+        login()
+        write_config()
+    if config["remove_plants_by_rewarded_ad"]["enable"]:
+        remove_plants_by_rewarded_ad()
+    Avalon.info("所有任务执行完毕~", front="\n")
+
+
+if __name__ == '__main__':
+    config = {}
+    plants = []
+    coin_tree_types = {}
+    username = ""
+    passwd = ""
+    remember_token = ""
+    uid = 0
+    os.chdir(sys.path[0])
+    main()
+    sys.exit(0)
